0a8e036c859e228eb95f71ba90ea1736385b1061
王建平 <wjp19951024@163.com>
2019-08-13 17:01:23 +0800

    Node

--- c++/hello/hello.cc
+++ c++/hello/hello.cc
@@ -1,3 +1,30 @@
 #include <node.h>;
 #include <v8.h>
 #include <iostream>
+
+namespace hello
+{
+    using v8 :: FunctionCallBackInfo;
+    using v8 :: Isolate;
+    using v8 :: Local;
+    using v8 :: Object;
+    using v8 :: String;
+    using v8 :: Value;
+    using v8 :: Number;
+
+    void Method1 (const FunctionCallBackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        args.GetReturnValue().Set(String :: NewFromUtf8(isolate, 'hello world'));
+    }
+
+    void Method2 (const FunctionCallBackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        Local<Number> value = Local<Number> :: Cast(args[0]);
+        double num = value -> NumberValue() + 1;
+
+        char buf[128] = {0};
+        sprintf(buf, '%f', num);
+
+        args.GetReturnValue().Set(String :: NewFromUtf8(isolate, buf));
+    }
+} // namespace hello
