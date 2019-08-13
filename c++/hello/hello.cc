489a85e787065139579b36dee0b4d4cb8e8ca78c
王建平 <wjp19951024@163.com>
2019-08-13 20:57:59 +0800

    add

--- c++/hello/hello.cc
+++ c++/hello/hello.cc
@@ -1,10 +1,10 @@
-#include <node.h>;
+#include <node.h>
 #include <v8.h>
 #include <iostream>
 
 namespace hello
 {
-    using v8 :: FunctionCallBackInfo;
+    using v8 :: FunctionCallbackInfo;
     using v8 :: Isolate;
     using v8 :: Local;
     using v8 :: Object;
@@ -12,19 +12,27 @@ namespace hello
     using v8 :: Value;
     using v8 :: Number;
 
-    void Method1 (const FunctionCallBackInfo<Value>& args) {
+    void Method1 (const FunctionCallbackInfo<Value>& args) {
         Isolate* isolate = args.GetIsolate();
-        args.GetReturnValue().Set(String :: NewFromUtf8(isolate, 'hello world'));
+        args.GetReturnValue().Set(String :: NewFromUtf8(isolate, "hello world"));
     }
 
-    void Method2 (const FunctionCallBackInfo<Value>& args) {
+    void Method2 (const FunctionCallbackInfo<Value>& args) {
         Isolate* isolate = args.GetIsolate();
-        Local<Number> value = Local<Number> :: Cast(args[0]);
+        Local<Number> value = Local<Number>::Cast(args[0]);
+        // double num = value->NumberValue() + 1;
         double num = value -> NumberValue() + 1;
 
         char buf[128] = {0};
-        sprintf(buf, '%f', num);
+        sprintf(buf, "%f", num);
 
         args.GetReturnValue().Set(String :: NewFromUtf8(isolate, buf));
     }
+
+    void init (Local<Object> exports) {
+        NODE_SET_METHOD(exports, "hello1", Method1);
+        NODE_SET_METHOD(exports, "addone", Method2);
+    }
+
+    NODE_MODULE(hello, init)
 } // namespace hello
