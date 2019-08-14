459ff1ff33d25c3230e5f0ebf7fcaf8c4b3a500b
王建平 <wjp19951024@163.com>
2019-08-14 17:49:35 +0800

    msg

--- shell/git.sh
+++ shell/git.sh
@@ -1,20 +1,40 @@
 #! /bin/sh
 
-echo -n "分支名称: $@"
-msg=$1
-echo $0
-echo $1
-echo "================"
-if [ -n "${msg}" ]; then
-    cd $0 && ls
-    git add .
-    git commit -m "${msg}"
-    git pull
-    # git status
-    git push
-    echo "本地提交完成"
-else
-    "添加注释"
-fi
+echo "================start"
+# echo -n "分支名称: $@"
+# msg=$2
+# echo $1
+# echo $2
 
+# if [ -n "${msg}" ]; then
+#     cd $1 && ls
+#     git add .
+#     git commit -m "${msg}"
+#     git pull
+#     # git status
+#     git push
+#     echo "本地提交完成"
+# else
+#     "添加注释"
+# fi
+echo "$@"
+# echo "$0"
+echo "$1"
+echo "$2"
+echo "--------==>"
+
+path=$1
+msg=$2
+echo "path: ${path}"
+echo "msg: ${msg}"
+echo "--------==>"
+# cd ${path} && ls
+cd ${path}
+echo "进入${path}"
+echo "--------==>"
+
+git add .
+git commit -m "${msg}"
+echo "文件已暂存"
+echo "--------==>>"
 echo "============>>>>end"
