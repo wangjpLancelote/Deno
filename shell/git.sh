d4875241b822354221e9604cb450d61b146ce678
王建平 <wjp19951024@163.com>
2019-08-14 17:23:39 +0800

    /Users/wangjianping/Deno

--- shell/git.sh
+++ shell/git.sh
@@ -1,13 +1,19 @@
 #! /bin/sh
 
 echo -n "分支名称: $@"
-msg = $1;
-echo $@
-cd $@ && ls
+msg=$1;
+echo $0
 echo "================"
-git add .
-git commit -m "${msg}"
-git pull --rebase
-git status
-git push
-echo "end"
+if [ -n "${msg}" ]; then
+    cd $0 && ls
+    git add .
+    git commit -m "${msg}"
+    git pull
+    git status
+    git push
+    echo "本地提交完成"
+else
+    "添加注释"
+fi
+
+echo "============>>>>end"
