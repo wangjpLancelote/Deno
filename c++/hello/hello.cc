64403f31744128b8f137cfc5258dc52259ab9555
wangjp19951024 <wjp19951024@163.com>
2019-09-17 23:07:56 +0800

    cpp

--- c++/hello/hello.cc
+++ c++/hello/hello.cc
@@ -1,3 +1,17 @@
 #include <node.h>;
 #include <v8.h>
 #include <iostream>
+
+namespace demo {
+    using v8 :: FunctionCallBackInfo;
+    using v8 :: Isolate;
+    using v8 :: Local;
+    using v8 :: Object;
+    using v8 :: String;
+    using v8 :: Value;
+
+    void Method (const FunctionCallBackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        args.GetReturnValue().Set(String :: NewFromUtf8(isolate, 'world'));
+    }
+}
